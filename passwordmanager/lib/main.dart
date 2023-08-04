7f3515667bd99f7011429af1ae5c3866cf7141d3
aaku111 <aakash4455logicseed@gmail.com>
2023-08-04 21:03:09 +0545

    first commit

---
+++ passwordmanager/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:passwordmanager/homepage.dart';
+import 'package:passwordmanager/view/loginview/loginview.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginView(),
+  ));
+}
